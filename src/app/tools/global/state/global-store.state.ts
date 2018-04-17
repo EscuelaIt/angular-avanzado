a05ed5630b6c22297a731959e8b1959a23ccb3a9
Alberto Basalo <albertobasalo@hotmail.com>
2018-04-17 10:28:20 +0200

    select observable

--- src/app/tools/global/state/global-store.state.ts
+++ src/app/tools/global/state/global-store.state.ts
@@ -1,25 +1,25 @@
 import { Injectable } from "@angular/core";
-import { globalInitialState } from "@tools/global/state/models/global.model";
-import { BehaviorSubject, Observable, Subject } from "rxjs";
+import {
+	globalInitialState,
+	Global
+} from "@tools/global/state/models/global.model";
+import { Observable, Subject } from "rxjs";
 import { Action } from "@tools/global/state/global-store.actions";
 import { globalStoreReducer } from "@tools/global/state/global-store.reducer";
 
 @Injectable()
 export class GlobalStore {
-	private state = globalInitialState;
+	private state: Global;
 
 	private userMessage$ = new Subject<string>();
-	private userToken$ = new BehaviorSubject<string>("");
+	private userToken$ = new Subject<string>();
+
+	public selectUserMessage$ = (): Observable<string> =>
+		this.userMessage$.asObservable();
+	public selectUserToken$ = (): Observable<string> =>
+		this.userToken$.asObservable();
 
 	constructor() {}
-
-	public selectUserMessage$(): Observable<string> {
-		return this.userMessage$.asObservable();
-	}
-
-	public selectUserToken$(): Observable<string> {
-		return this.userToken$.asObservable();
-	}
 
 	public dispatch(action: Action) {
 		this.state = globalStoreReducer(this.state, action);
