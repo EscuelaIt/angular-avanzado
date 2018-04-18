a34399c2532b58c1b66a308e78b54d4c312311d9
Alberto Basalo <albertobasalo@hotmail.com>
2018-04-18 12:35:28 +0200

    /dist/angular-avanzado

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const path = require("path");
 
 const app = express();
 
-app.use(express.static(__dirname + "/dist"));
+app.use(express.static(__dirname + "/dist/angular-avanzado"));
 
 app.get("*", function(req, res) {
-	res.sendFile(path.join(__dirname + "/dist/index.html"));
+	res.sendFile(
+		path.join(__dirname + "/dist/angular-avanzado/index.html")
+	);
 });
 
 app.listen(process.env.PORT || 8080);
