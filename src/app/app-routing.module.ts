93492f3ff84fd4baf22f95f3748a8caad51eb613
Alberto Basalo <albertobasalo@hotmail.com>
2018-04-18 16:26:51 +0200

    @ paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { RouterModule, Routes } from "@angular/router";
 const routes: Routes = [
 	{
 		path: "",
-		loadChildren: "@routes/home/home.module#HomeModule"
+		loadChildren: "@home/home.module#HomeModule"
 	},
 	{
 		path: "about",
