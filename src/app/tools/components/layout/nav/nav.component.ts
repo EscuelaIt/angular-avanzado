76aa808c1097b8215590a31ee1f50a813a1c9958
Alberto Basalo <albertobasalo@hotmail.com>
2018-04-19 13:03:00 +0200

    tab navs

--- src/app/tools/components/layout/nav/nav.component.ts
+++ src/app/tools/components/layout/nav/nav.component.ts
@@ -2,17 +2,24 @@ import { Component, OnInit, Input } from "@angular/core";
 import { NavLink } from "@tools/models/nav-link.model";
 
 @Component({
-  selector: "ab-nav",
-  template: `
-  <nav>
-    <a class="button button-clear" *ngFor="let link of navLinks" routerLink="{{link.routerLink}}">{{link.caption}}</a>
+	selector: "ab-nav",
+	template: `
+  <nav mat-tab-nav-bar>
+    <a mat-tab-link 
+      *ngFor="let link of navLinks" 
+      routerLink="{{link.routerLink}}" 
+      routerLinkActive
+      #rla="routerLinkActive"
+      [active]="rla.isActive">
+        {{link.caption}}
+    </a>
   </nav>
   `,
-  styles: []
+	styles: []
 })
 export class NavComponent implements OnInit {
-  @Input() navLinks: NavLink[];
-  constructor() {}
+	@Input() navLinks: NavLink[];
+	constructor() {}
 
-  ngOnInit() {}
+	ngOnInit() {}
 }
